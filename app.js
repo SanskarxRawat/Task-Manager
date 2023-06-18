25da3819429cf7a7cd6dc34ef43e55b226050e71
SanskarRawatfc <130359484+SanskarRawatfc@users.noreply.github.com>
2023-06-18 23:22:31 +0530

    "removed error"

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const app = express();
 const connectDB = require('./db/connection')
 require('dotenv').config();
 const tasks = require('./routes/tasks');
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware=require('./middleware/error-handler');
 const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.static('./public'));
 app.use(express.json());
-app.use(notFound);
-app.use(errorHandlerMiddleware);
+
 
 //routes
 app.use("/api/v1/tasks", tasks.routes);
--- controller/tasks.js
+++ controller/tasks.js
@@ -1,48 +1,65 @@
 const Task = require('../models/tasks');
-const asyncWrapper = require('../middleware/async');
-const {createCustomError}=require('../errors/custom-error')
 
-
-const getAllTasks = asyncWrapper(async (req, res) => {
-    const task = await Task.find({});
-    res.status(200).json({ status: 'success', data: task });
-})
-
-const createTask = asyncWrapper(async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({ status: 'success', data: task });
-
-})
-
-const getTask = async (req, res,next) => {
-
-    const { id: taskId } = req.params;
-    const task = await Task.findOne({ _id: taskId });
-    if (!task){
-        return  next(createCustomError(`No task with the given id : ${taskId}`,404));
+const getAllTasks = async (req, res) => {
+    try {
+        const task = await Task.find({});
+        res.status(200).json({ status: 'success', data: task });
+    } catch (error) {
+        res.send(500).json({msg:error});
     }
-    res.status(201).json({ status: 'success', data: task });
+
 }
-const updateTask = asyncWrapper(async (req, res,next) => {
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ status: 'success', data: task });
+    }catch(error){
+        res.send(500).json({msg:error});
+    }
+}
+
+const getTask = async (req, res) => {
     const { id: taskId } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
-        new: true,
-        runValidators: true,
-    });
-    if (!task){
-        return  next(createCustomError(`No task with the given id : ${taskId}`,404));
-    }    res.status(201).json({ id: taskId, data: req.body });
-
-})
-
-const deleteTask = asyncWrapper(async (req, res,next) => {
+    try{
+        const task = await Task.findOne({ _id: taskId });
+        if (!task) {
+            return res.status(404).json(`No task with the given id : ${taskId}`);
+        }
+        res.status(201).json({ status: 'success', data: task });
+    }catch(error){
+        res.send(500).json({msg:error});
+    }
+}
+const updateTask = async (req, res) => {
     const { id: taskId } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskId });
-    if (!task){
-        return  next(createCustomError(`No task with the given id : ${taskId}`,404));
-    }    res.status(201).json({ status: 'success', data: task });
+    try{
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(201).json({ id: taskId, data: req.body });
+        if (!task) {
+            return res.status(404).json(`No task with the given id : ${taskId}`);
+        }
+    }catch(error){
+        res.send(500).json({msg:error});
+    }
+    
+}
 
-})
+const deleteTask = async (req, res, next) => {
+    const { id: taskId } = req.params;
+    try{
+        const task = await Task.findOneAndDelete({ _id: taskId });
+        if (!task) {
+            return res.status(404).json(`No task with the given id : ${taskId}`);
+        } 
+        res.status(201).json({ status: 'success', data: task });
+    }catch(error){
+        res.send(500).json({msg:error});
+    }
+}
 
 module.exports = {
     getAllTasks, createTask, getTask, updateTask, deleteTask
