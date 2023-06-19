17f7849748f2c2d495dbbe9507fa68e45f19a54c
SanskarRawatfc <130359484+SanskarRawatfc@users.noreply.github.com>
2023-06-19 12:07:29 +0530

    "cors added"

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./db/connection')
+const connectDB = require('./db/connection');
+const cors = require('cors');
 require('dotenv').config();
 const tasks = require('./routes/tasks');
 const port = process.env.PORT || 3000;
 
 //middleware
+app.use(cors());
 app.use(express.static('./public'));
 app.use(express.json());
 
